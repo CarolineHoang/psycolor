88116943e0ffbe6df71697ec33a9e895dc5f3a82
Cremebrutale <caroline.hoang@columbiaspectator.com>
2019-11-02 20:59:20 -0400

    Updates

---
+++ precache-manifest.c7e67507f288d360d43d74207a0295bc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad14cccfb5f9ad6e7ae0dfad24d3cca5",
+    "url": "/psycolor/index.html"
+  },
+  {
+    "revision": "dad57027fff205d46aa2",
+    "url": "/psycolor/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "e46a94a894c691e06f4a",
+    "url": "/psycolor/static/js/2.782608b2.chunk.js"
+  },
+  {
+    "revision": "dad57027fff205d46aa2",
+    "url": "/psycolor/static/js/main.766e1255.chunk.js"
+  },
+  {
+    "revision": "964e7bd5a667d7ee9111",
+    "url": "/psycolor/static/js/runtime-main.7f1bea74.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/psycolor/static/media/logo.25bf045c.svg"
+  }
+]);
